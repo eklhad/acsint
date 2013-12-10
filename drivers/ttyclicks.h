41da7c6431659c8e13f86e98b9e2667d629dff32
Karl Dahlke <eklhad@comcast.net>
2013-12-10 17:06:32 -0500

    Comment / reorder externs.

--- drivers/ttyclicks.h
+++ drivers/ttyclicks.h
@@ -2,15 +2,16 @@
 #define _LINUX_TTYCLICKS_H
 
 /* External prototypes for ttyclicks.c */
+/* See Documentation/accessibility/ttyclicks.txt for documentation. */
 
 extern bool ttyclicks_on;	/* speaker sounds activated */
 extern bool ttyclicks_tty;	/* speaker sounds from tty */
 extern bool ttyclicks_kmsg;	/* speaker sounds from kernel message */
 
-void ttyclicks_notes(const short *a);
-void ttyclicks_steps(int start, int end, int step, int duration);
-void ttyclicks_bell(void);
 void ttyclicks_click(void);
 void ttyclicks_cr(void);
+void ttyclicks_bell(void);
+void ttyclicks_notes(const short *notelist);
+void ttyclicks_steps(int start, int end, int step, int duration);
 
 #endif
